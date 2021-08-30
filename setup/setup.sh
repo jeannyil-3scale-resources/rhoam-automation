047eb316aa821253b52d522f46beab1b0cc7dea9
Jean Armand Nyilimbibi <jean.nyilimbibi@gmail.com>
2021-08-31 00:50:16 +0200

    Added camel-quarkus-rhoam-webhook-handler-api Publish to 3sclae CI/CD pipeline

--- setup/setup.sh
+++ setup/setup.sh
@@ -66,13 +66,31 @@ oc new-app -f cicd-3scale/3scaletoolbox/camel-quarkus-jsonvalidation-api/pipelin
 -p DEV_PROJECT=$DEV_PROJECT \
 -p TEST_PROJECT=$TEST_PROJECT \
 -p PROD_PROJECT=$PROD_PROJECT \
--p TARGET_INSTANCE=3scale-rhpds \
+-p TARGET_INSTANCE="rhpds-3scale-tenant" \
 -p PUBLIC_PRODUCTION_WILDCARD_DOMAIN=apps.jeannyil.sandbox452.opentlc.com \
 -p PUBLIC_STAGING_WILDCARD_DOMAIN=staging.apps.jeannyil.sandbox452.opentlc.com \
 -p OIDC_ISSUER_ENDPOINT="https://<CLIENT_ID>:<CLIENT_SECRET>@<HOST>:<PORT>/auth/realms/<REALM_NAME>" \
--p DEVELOPER_ACCOUNT_ID=3 \
+-p DEVELOPER_ACCOUNT_ID="john" \
 -p BASIC_PLAN_YAML_FILE_PATH="https://raw.githubusercontent.com/jeannyil-rhoam-resources/rhoam-automation/main/cicd-3scale/3scaletoolbox/camel-quarkus-jsonvalidation-api/basic-plan.yaml" \
 -p UNLIMITED_PLAN_YAML_FILE_PATH="https://raw.githubusercontent.com/jeannyil-rhoam-resources/rhoam-automation/main/cicd-3scale/3scaletoolbox/camel-quarkus-jsonvalidation-api/unlimited-plan.yaml" \
+-p DISABLE_TLS_VALIDATION="no" \
+-p TOOLBOX_IMAGE_REGISTRY="image-registry.openshift-image-registry.svc:5000/rh-dev/toolbox-rhel7:3scale2.10"
+
+echo "import camel-quarkus-rhoam-webhook-handler-api 3Scale API publishing pipeline"
+oc new-app -f cicd-3scale/3scaletoolbox/camel-quarkus-rhoam-webhook-handler-api/pipeline-template.yaml \
+-p GIT_REPO="https://github.com/jeannyil-rhoam-resources/rhoam-automation.git" \
+-p GIT_BRANCH="main" \
+-p IMAGE_NAMESPACE=$DEV_PROJECT \
+-p DEV_PROJECT=$DEV_PROJECT \
+-p TEST_PROJECT=$TEST_PROJECT \
+-p PROD_PROJECT=$PROD_PROJECT \
+-p TARGET_INSTANCE="rhpds-3scale-tenant" \
+-p PUBLIC_PRODUCTION_WILDCARD_DOMAIN=apps.jeannyil.sandbox452.opentlc.com \
+-p PUBLIC_STAGING_WILDCARD_DOMAIN=staging.apps.jeannyil.sandbox452.opentlc.com \
+-p OIDC_ISSUER_ENDPOINT="https://<CLIENT_ID>:<CLIENT_SECRET>@<HOST>:<PORT>/auth/realms/<REALM_NAME>" \
+-p DEVELOPER_ACCOUNT_ID="john" \
+-p BASIC_PLAN_YAML_FILE_PATH="https://raw.githubusercontent.com/jeannyil-rhoam-resources/rhoam-automation/main/cicd-3scale/3scaletoolbox/camel-quarkus-rhoam-webhook-handler-api/basic-plan.yaml" \
+-p UNLIMITED_PLAN_YAML_FILE_PATH="https://raw.githubusercontent.com/jeannyil-rhoam-resources/rhoam-automation/main/cicd-3scale/3scaletoolbox/camel-quarkus-rhoam-webhook-handler-api/unlimited-plan.yaml" \
 -p DISABLE_TLS_VALIDATION="no" \
 -p TOOLBOX_IMAGE_REGISTRY="image-registry.openshift-image-registry.svc:5000/rh-dev/toolbox-rhel7:3scale2.10"
 

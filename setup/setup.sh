dac7cb63d018905c0335a9be75fa10f395e5b0e3
Jean Armand Nyilimbibi <jean.nyilimbibi@gmail.com>
2021-09-04 15:40:56 +0200

    Updated setup

--- setup/setup.sh
+++ setup/setup.sh
@@ -67,8 +67,8 @@ oc new-app -f cicd-3scale/3scaletoolbox/camel-quarkus-jsonvalidation-api/pipelin
 -p TEST_PROJECT=$TEST_PROJECT \
 -p PROD_PROJECT=$PROD_PROJECT \
 -p TARGET_INSTANCE="rhpds-3scale-tenant" \
--p PUBLIC_PRODUCTION_WILDCARD_DOMAIN=apps.jeannyil.sandbox452.opentlc.com \
--p PUBLIC_STAGING_WILDCARD_DOMAIN=staging.apps.jeannyil.sandbox452.opentlc.com \
+-p PUBLIC_PRODUCTION_WILDCARD_DOMAIN=apps.jeannyil.sandbox430.opentlc.com \
+-p PUBLIC_STAGING_WILDCARD_DOMAIN=staging.apps.jeannyil.sandbox430.opentlc.com \
 -p OIDC_ISSUER_ENDPOINT="https://<CLIENT_ID>:<CLIENT_SECRET>@<HOST>:<PORT>/auth/realms/<REALM_NAME>" \
 -p DEVELOPER_ACCOUNT_ID="john" \
 -p BASIC_PLAN_YAML_FILE_PATH="https://raw.githubusercontent.com/jeannyil-rhoam-resources/rhoam-automation/main/cicd-3scale/3scaletoolbox/camel-quarkus-jsonvalidation-api/basic-plan.yaml" \
@@ -85,8 +85,8 @@ oc new-app -f cicd-3scale/3scaletoolbox/camel-quarkus-rhoam-webhook-handler-api/
 -p TEST_PROJECT=$TEST_PROJECT \
 -p PROD_PROJECT=$PROD_PROJECT \
 -p TARGET_INSTANCE="rhpds-3scale-tenant" \
--p PUBLIC_PRODUCTION_WILDCARD_DOMAIN=apps.jeannyil.sandbox452.opentlc.com \
--p PUBLIC_STAGING_WILDCARD_DOMAIN=staging.apps.jeannyil.sandbox452.opentlc.com \
+-p PUBLIC_PRODUCTION_WILDCARD_DOMAIN=apps.jeannyil.sandbox430.opentlc.com \
+-p PUBLIC_STAGING_WILDCARD_DOMAIN=staging.apps.jeannyil.sandbox430.opentlc.com \
 -p OIDC_ISSUER_ENDPOINT="https://<CLIENT_ID>:<CLIENT_SECRET>@<HOST>:<PORT>/auth/realms/<REALM_NAME>" \
 -p DEVELOPER_ACCOUNT_ID="john" \
 -p BASIC_PLAN_YAML_FILE_PATH="https://raw.githubusercontent.com/jeannyil-rhoam-resources/rhoam-automation/main/cicd-3scale/3scaletoolbox/camel-quarkus-rhoam-webhook-handler-api/basic-plan.yaml" \

986fba39bacc93590f753e2d4651bb10d7071cbf
Jean Armand Nyilimbibi <jean.nyilimbibi@gmail.com>
2021-09-05 13:13:25 +0200

    Updated setup

--- setup/setup.sh
+++ setup/setup.sh
@@ -38,6 +38,13 @@ oc policy add-role-to-user admin system:serviceaccount:${DEV_PROJECT}:jenkins -n
 # /!\ If Jenkins Instance is installed using the  Jenkins Operator
 # oc policy add-role-to-user admin system:serviceaccount:${DEV_PROJECT}:jenkins-persistent -n ${DEV_PROJECT}
 
+echo "import camel-quarkus-fruits-and-legumes-api CI/CD build pipeline"
+oc new-app -f cicd-api-build/camel-quarkus-fruits-and-legumes-api/build-deploy-pipeline.yml \
+-p IMAGE_NAMESPACE=$DEV_PROJECT \
+-p DEV_PROJECT=$DEV_PROJECT \
+-p TEST_PROJECT=$TEST_PROJECT \
+-p PROD_PROJECT=$PROD_PROJECT
+
 echo "import camel-quarkus-jsonvalidation-api CI/CD build pipeline"
 oc new-app -f cicd-api-build/camel-quarkus-jsonvalidation-api/build-deploy-pipeline.yml \
 -p IMAGE_NAMESPACE=$DEV_PROJECT \
@@ -57,6 +64,24 @@ oc new-app -f cicd-api-build/camel-quarkus-rhoam-webhook-handler-api/build-deplo
 
 # echo "import integration-master-pipeline"
 # TODO
+
+echo "import camel-quarkus-fruits-and-legumes-api 3Scale API publishing pipeline"
+oc new-app -f cicd-3scale/3scaletoolbox/camel-quarkus-fruits-and-legumes-api/pipeline-template.yaml \
+-p GIT_REPO="https://github.com/jeannyil-rhoam-resources/rhoam-automation.git" \
+-p GIT_BRANCH="main" \
+-p IMAGE_NAMESPACE=$DEV_PROJECT \
+-p DEV_PROJECT=$DEV_PROJECT \
+-p TEST_PROJECT=$TEST_PROJECT \
+-p PROD_PROJECT=$PROD_PROJECT \
+-p TARGET_INSTANCE="rhpds-3scale-tenant" \
+-p PUBLIC_PRODUCTION_WILDCARD_DOMAIN=apps.jeannyil.sandbox430.opentlc.com \
+-p PUBLIC_STAGING_WILDCARD_DOMAIN=staging.apps.jeannyil.sandbox430.opentlc.com \
+-p OIDC_ISSUER_ENDPOINT="https://<CLIENT_ID>:<CLIENT_SECRET>@<HOST>:<PORT>/auth/realms/<REALM_NAME>" \
+-p DEVELOPER_ACCOUNT_ID="john" \
+-p BASIC_PLAN_YAML_FILE_PATH="https://raw.githubusercontent.com/jeannyil-rhoam-resources/rhoam-automation/main/cicd-3scale/3scaletoolbox/camel-quarkus-fruits-and-legumes-api/basic-plan.yaml" \
+-p UNLIMITED_PLAN_YAML_FILE_PATH="https://raw.githubusercontent.com/jeannyil-rhoam-resources/rhoam-automation/main/cicd-3scale/3scaletoolbox/camel-quarkus-fruits-and-legumes-api/unlimited-plan.yaml" \
+-p DISABLE_TLS_VALIDATION="no" \
+-p TOOLBOX_IMAGE_REGISTRY="image-registry.openshift-image-registry.svc:5000/rh-dev/toolbox-rhel7:3scale2.10"
 
 echo "import camel-quarkus-jsonvalidation-api 3Scale API publishing pipeline"
 oc new-app -f cicd-3scale/3scaletoolbox/camel-quarkus-jsonvalidation-api/pipeline-template.yaml \
